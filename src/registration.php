28c8b9ac7de52179cd6441eea4114bd4ff6913c7
Ilja Lapkovskis <ilja@scandi.com>
2019-08-01 12:42:38 +0300

    Set require to magento2-base

--- src/registration.php
+++ src/registration.php
@@ -1,8 +1,12 @@
 <?php
 /**
- * @author    Raivis Dejus <info@scandiweb.com>
- * @copyright Copyright (c) 2018 Scandiweb, Ltd (https://scandiweb.com)
+ * @category  ScandiPWA
+ * @package   ScandiPWA\Sitemap
+ * @author    Ilja Lapkovskis <info@scandiweb.com / ilja@scandiweb.com>
+ * @copyright Copyright (c) 2019 Scandiweb, Ltd (http://scandiweb.com)
+ * @license   OSL-3.0
  */
+
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
